81808d5c603e879acbe98dde0260ae6ef7ba0933
priya shukla <priyashukla22@navgurukul.org>
2024-02-15 19:46:38 +0530

    first commit

---
+++ movi.js
@@ -0,0 +1,19 @@
+// const { response } = require("express")
+
+// const apiUrl=`http://www.omdbapi.com/?t=ABCD&apikey=c37526bb`
+// fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/close`)
+//      .then(response=>response.json())
+//      .then(data=>{
+//         console.log(data);
+//      })
+//      .catch(error =>{
+//         console.log("error")
+//      })
+
+
+
+
+
+
+
+
